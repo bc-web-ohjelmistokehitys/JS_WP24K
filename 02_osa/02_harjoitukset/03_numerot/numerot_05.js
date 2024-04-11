c1817ead2541e89ea2801633200965e27398102d
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-04-11 13:47:15 +0300

    Update numerot_05.js

--- 02_osa/02_harjoitukset/03_numerot/numerot_05.js
+++ 02_osa/02_harjoitukset/03_numerot/numerot_05.js
@@ -1,6 +1,6 @@
 /** Tehtävä: Ostoskori
  * Kuvitellaan, että olet kehittämässä yksinkertaista verkkokauppasovellusta. Sinun tehtäväsi on luoda ohjelma, joka laskee ostoskorissa olevien tuotteiden yhteishinnan ja tulostaa sen.
- * - Määrittele muuttujat kullekin ostoskorissa oleville tuotteille ja niiden hinnoille.
+ * - Määrittele muuttujat kullekin ostoskorissa olevalle tuotteelle ja niiden hinnoille.
  * - Laske tuotteiden yhteishinta ja tallenna se muuttujaan, esimerkiksi yhteishinta.
  * - Tulosta yhteishinta konsoliin.
  */
