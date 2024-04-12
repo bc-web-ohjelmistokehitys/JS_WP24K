d35e2cac3eb9b1839ae3fc4a156a8cc98a8a463b
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-04-12 17:46:17 +0300

    removed silmukat_03 solution for now

--- 03_osa/03_harjoitukset/silmukat/silmukat_03.js
+++ 03_osa/03_harjoitukset/silmukat/silmukat_03.js
@@ -1,17 +1,5 @@
 // Luo ohjelma, joka pyytää jatkuvasti käyttäjää syöttämään etäisyyden (kilometreinä) ja ajan (tunteina) ja laskee sitten keskinopeuden. 
 //Ohjelman tulisi lopettaa, kun käyttäjä syöttää 0 etäisyydeksi. Saadessaan etäisyyden 0 ohjelman ei pitäisi pyytää enempää syöttöä.
-
+// Tehdään tämä opettajan esimerkkinä
 let etaisyys, aika;
 
-while (etaisyys !== 0) {
-  etaisyys = Number(prompt('Syötä etäisyys kilometreinä'));
-
-  if (etaisyys === 0) {
-    console.log('Etäisyys on 0 - peli päättyy');
-    break;
-  }
-
-  aika = Number(prompt('Syötä aika tunteina'));
-  let keskinopeus = etaisyys / aika;
-  console.log('Keskinopeus on:', keskinopeus);
-}
