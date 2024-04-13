98886c89cf802c8041ce9d94dec4018bd2505408
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-04-13 17:17:59 +0300

    Added dom execises

---
+++ 04_osa/04_harjoitukset/DOM_harjoitukset/DOM_1/script.js
@@ -0,0 +1,4 @@
+/* 
+Kirjoita JavaScript-koodi, joka valitsee sisällön tunnuksen käyttämällä getElementById-metodia.
+Kirjoita funktio, joka päivittää valitun elementin tekstisisällön painamalla nappia tekstiksi "Hei, maailma!".
+*/
---
+++ 04_osa/04_harjoitukset/DOM_harjoitukset/DOM_2/script.js
@@ -0,0 +1,10 @@
+/* 
+Kirjoita JavaScript-koodi, joka lisää tapahtumankäsittelijän napille.
+
+Kun nappia klikataan, funktion tulisi:
+- Hakea arvo syötekentästä.
+- Luoda uusi <li> elementti ja asettaa sen tekstisisältö syötekentän arvoksi.
+- Lisätä uusi <li> elementti hedelmälistan <ul>-elementtiin.
+
+Varmista, että syötekenttä tyhjennetään hedelmän lisäämisen jälkeen.
+*/
