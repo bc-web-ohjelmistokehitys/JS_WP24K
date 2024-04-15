04579c1a40c01d4a6173383e51372dfc187a8313
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-04-15 14:59:17 +0300

    fixed typo, added clarification

--- 03_osa/03_harjoitukset/silmukat/silmukat_02.js
+++ 03_osa/03_harjoitukset/silmukat/silmukat_02.js
@@ -1,2 +1,3 @@
-//Tee ohjelma, joka tulostaa kaikki parilliset kokonaisluvut 1 ja 99 välillä, esittäen ne seuraavalla tavalla
-//2, 98, 4, 96, 6, 62 ja niin edelleen. Tulosta luvut samalle riville. 
+// Tee ohjelma, joka tulostaa kaikki parilliset kokonaisluvut 1 ja 99 välillä, esittäen ne seuraavalla tavalla
+// 2, 98, 4, 96, 6, 94 ja niin edelleen. Tulosta luvut samalle riville. 
+// Saat itse valita käytkö luvut läpi kaksi kertaa vai lopetatko lukuun 50.
