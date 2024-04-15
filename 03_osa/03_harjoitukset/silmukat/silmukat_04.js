6e8ff5dc3a7bde579236fd612b248229e335f1ec
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-04-15 10:53:41 +0300

    Update silmukat_04.js

--- 03_osa/03_harjoitukset/silmukat/silmukat_04.js
+++ 03_osa/03_harjoitukset/silmukat/silmukat_04.js
@@ -1,18 +1,3 @@
 // Kehitä ohjelma, joka pyytää käyttäjää syöttämään 20 lukua. Kun kaikki luvut on syötetty, 
 //ohjelman tulisi näyttää, kuinka monta näistä luvuista on parillisia.
 // Koska käytössä on prompt, tämä toimii selaimessa html-sivun kautta. 
-
-let summa = 0;
-let laskuri = 0;
-let parillisetLuvut = 0;
-
-do {
-  let syote = Number(prompt('Syötä luku'));
-  summa += syote;
-  laskuri++;
-  if (syote % 2 === 0) {
-    parillisetLuvut++;
-  }
-} while (laskuri !== 20);
-
-console.log(`Parillisia lukuja oli ${parillisetLuvut}`);
