e928d7e241c90a9e599f0f533e292748fd22b140
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-04-18 11:58:52 +0300

    Update silmukat_07.js

--- 03_osa/03_harjoitukset/silmukat/silmukat_07.js
+++ 03_osa/03_harjoitukset/silmukat/silmukat_07.js
@@ -1,2 +1,9 @@
-//Tee ohjelma, joka kysyy kymmentä numeroa. Ohjelma laskee ja tulostaa summan ja keskiarvon sekä 
-//tulostaa pienimmän ja suurimman numeron.
+// Tee ohjelma, joka kysyy kymmentä numeroa. Ohjelma laskee ja tulostaa summan ja keskiarvon sekä 
+// tulostaa pienimmän ja suurimman numeron.
+
+/* VINKKI: talleta annetut luvut taulukkoon, jotta saat niistä suurimman ja pienimmän arvon 
+Esimerkki taulukon määrittely ennen silmukkaa näin 
+let numerot = []
+taulukkoon arvojen lisääminen silmukan sisällä toimii näin
+numerot += annettuArvo 
+Käydään taulukot läpi osassa 5. */
