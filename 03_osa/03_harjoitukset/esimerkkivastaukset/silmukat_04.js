63ea52e9fad415d025e50e0a7dd5dd1f3c8cc01f
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-04-18 12:06:11 +0300

    examples of silmukat 4

---
+++ 03_osa/03_harjoitukset/esimerkkivastaukset/silmukat_04.js
@@ -0,0 +1,32 @@
+// Kehitä ohjelma, joka pyytää käyttäjää syöttämään 20 lukua. Kun kaikki luvut on syötetty, 
+// ohjelman tulisi näyttää, kuinka monta näistä luvuista on parillisia.
+
+let parilliset = 0;
+
+for (let kertalaskuri = 0; kertalaskuri < 6; kertalaskuri++) {
+  let syote = Number(prompt("syötä numero"))
+  console.log("syöte on:",syote);
+
+  if (syote % 2 == 0){
+    parilliset++
+  }
+}
+console.log("parillisten määrä:",parilliset)
+
+
+
+let laskuri = 0
+let parillisetLuvut = 0
+
+do {
+  let input = Number(prompt("Anna lisää lukuja"))
+  if (input % 2 == 0){
+    parillisetLuvut++
+  }
+  laskuri++
+
+} while (laskuri != 20)
+
+console.log("Parillisia lukuja oli " + parillisetLuvut)
+
+
